a31bcfc5841060a55827712c5dccfbf6331c4ede
Z C <maraleily@gmail.com>
2025-07-24 06:55:53 +0000

    added descriptions to stg_jaffle_shop__customers and stg_jaffle_shop__orders

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -18,14 +18,14 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        -- sum(amount) as lifetime_value
 
     from orders
 
     group by 1
 
 ),
-
 final as (
 
     select
@@ -34,7 +34,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        -- customer_payments.lifetime_value
 
     from customers
 
